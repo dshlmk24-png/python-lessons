b17d219536f14878217785832cf0914fb6b5640a
Daria Sholomok <dshlmk24@gmail.com>
2026-08-30 16:39:51 +0300

    lessoon06K

---
+++ lesson_06K/test_03_shop.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_shop():
+    driver = webdriver.Firefox()
+    driver.get("https://www.saucedemo.com/")
+
+    driver.find_element(By.ID, "user-name").send_keys(
+        "standard_user"
+    )
+    driver.find_element(By.ID, "password").send_keys(
+        "secret_sauce"
+    )
+    driver.find_element(By.ID, "login-button").click()
+
+    driver.find_element(
+        By.ID, "add-to-cart-sauce-labs-backpack"
+    ).click()
+    driver.find_element(
+        By.ID, "add-to-cart-sauce-labs-bolt-t-shirt"
+    ).click()
+    driver.find_element(
+        By.ID, "add-to-cart-sauce-labs-onesie"
+    ).click()
+    driver.find_element(
+        By.CLASS_NAME, "shopping_cart_link"
+    ).click()
+    driver.find_element(
+        By.ID, "checkout"
+    ).click()
+    driver.find_element(
+        By.ID, "first-name"
+    ).send_keys("Даша")
+    driver.find_element(
+        By.ID, "last-name"
+    ).send_keys("Шоломок")
+    driver.find_element(
+        By.ID, "postal-code"
+    ).send_keys("123456")
+    driver.find_element(
+        By.ID, "continue"
+    ).click()
+    total = driver.find_element(
+        By.CLASS_NAME, "summary_total_label"
+    ).text
+    driver.quit()
+    assert total == "Total: $58.29"
